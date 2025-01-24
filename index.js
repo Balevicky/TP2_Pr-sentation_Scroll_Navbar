a0dbb8881d54102e78f6cfe90afd73f4888f3d0a
Balevicky <balevictorien@yahoo.fr>
2025-01-24 16:11:57 +0000

    added code to index.js and modified style.css

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+const nav = document.querySelector("nav");
+let scrollLast = 0;
+
+window.addEventListener("scroll", (e) => {
+  // console.log(window.scrollY);
+  if (window.scrollY < scrollLast) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-60px";
+  }
+
+  scrollLast = window.scrollY;
+});
